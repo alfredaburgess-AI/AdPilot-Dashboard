6cd55a63bd99738e087c3f0ebed700b3ab7d5463
Alfred Burgess <your_email@example.com>
2026-03-28 15:14:50 -0400

    Pass --global.developmentMode false to fix App Runner issue

--- run_app.py
+++ run_app.py
@@ -4,5 +4,12 @@ import sys
 # Set PYTHONPATH environment variable to include the deps directory before exec
 os.environ['PYTHONPATH'] = '/app/deps:/app'
 
-# Exec into Python module streamlit
-os.execv(sys.executable, ['python3', '-m', 'streamlit', 'run', 'main.py', '--server.port', '8080', '--server.address', '0.0.0.0'])
+# Exec into Python module streamlit. Force global.developmentMode to false to avoid AssertionError.
+os.execv(sys.executable, [
+    'python3', '-m', 'streamlit', 'run', 'main.py',
+    '--global.developmentMode', 'false',
+    '--server.port', '8080',
+    '--server.address', '0.0.0.0',
+    '--server.headless', 'true',
+    '--server.enableCORS', 'false'
+])
